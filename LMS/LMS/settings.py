b677db8fb13701d52bc9f01e5e36b071a33f70b5
admin <admin@gmail.com>
2023-04-04 00:13:32 +0530

    first commit

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -116,3 +116,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ahireaditi27@gmail.com'
+EMAIL_HOST_PASSWORD = 'aditiahire@27'
